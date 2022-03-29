3029d77cd2fe64c1a49014b11258a303467854ed
tamasvar <41537421+tamasvar@users.noreply.github.com>
2022-03-29 10:02:01 +0200

    Add files via upload

---
+++ Diakok/Diakok/Services/StudentService.cs
@@ -0,0 +1,25 @@
+﻿using Diakok.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Diakok.Services
+{
+    public class StudentService
+    {
+        private readonly kkszkiContext context;
+
+        public StudentService(kkszkiContext context) { 
+            this.context = context;
+        }
+
+        public IEnumerable<Student> GetAllStudent()
+        {
+            return context.Students.Include(student=> student.Subjects).ToList();
+        }
+        public Student GetStudentByEmail(int id)
+        {
+            return context.Students.Include(s => s.Subjects).First(s=>s.Id == id);
+        }
+    }
+}
