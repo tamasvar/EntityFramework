282b465e72d037024d63e214c9e6fabdbf549157
tamasvar <41537421+tamasvar@users.noreply.github.com>
2022-03-30 09:33:16 +0200

    Add files via upload

---
+++ EntityFrameworkalap/EntityFrameworkalap/Services/StudentService.cs
@@ -0,0 +1,30 @@
+﻿
+using EntityFrameworkalap.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace EntityFrameworkalap.Services
+{
+    public class StudentService
+    {
+        private readonly kkszkiContext context;
+
+        public StudentService(kkszkiContext context)
+        {
+            this.context = context;
+        }
+
+        public IEnumerable<Student> GetAllStudents()
+        {
+            
+            return context.Students.Include(Student => Student.Subjects).Include(Student => Student.Grades).ToList();
+        }
+
+        public Student GetStudentByEmail(string email)
+        {
+            return context.Students.First(Student => Student.Email == email);
+        }
+        
+    }
+}
